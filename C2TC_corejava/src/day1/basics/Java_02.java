aff5d068812a8d8d1a2da9dbc175c66e0bbc8a94
omprakash1305 <ombshss2005@gmail.com>
2025-07-21 14:20:45 +0530

    2nd code

---
+++ C2TC_corejava/src/day1/basics/Java_02.java
@@ -0,0 +1,17 @@
+package day1.basics;
+
+class Java_02 {
+    public static void main(){
+        System.out.println("Welcomme form main 1");
+    }
+    public static void main(char c[]){
+        System.out.println("welcome form main 2");
+    }
+    public static void main(String ar){
+        System.out.println("welcome form main 3");
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Welcome 1..");    
+    }
+}
