fe0bef054d036cf0cb97eebcc2b0d19d6ee21a26
omprakash1305 <ombshss2005@gmail.com>
2025-07-21 14:42:39 +0530

    New one

---
+++ C2TC_corejava/src/day1/basics/java_03.java
@@ -0,0 +1,15 @@
+package day1.basics;
+import java.util.Scanner;
+
+public class java_03 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		int a= sc.nextInt();
+		int b=sc.nextInt();
+		System.out.print(a+b);
+
+	}
+
+}
