6ec3c7935a648dcf471cda67fd966bfbf792cd09
omprakash1305 <ombshss2005@gmail.com>
2025-07-21 18:49:09 +0530

    new one

---
+++ C2TC_corejava/src/day1/basics/java_05.java
@@ -0,0 +1,24 @@
+package day1.basics;
+
+public class java_05 {
+	public static void main(String[] args) {
+		char ch='1';
+		System.out.println(ch);
+		char ch1 = 65;
+		System.out.println(ch1);
+
+		
+		char var1 = '\u10A7';
+		System.out.println(var1);
+
+	
+		char var2 = '\u21AC';
+		System.out.println(var2);
+
+		
+		int a = 'B';
+		System.out.println(a);
+		
+		
+	}
+}
