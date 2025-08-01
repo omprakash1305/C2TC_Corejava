ff3619494d8bd163450502f80fb022860f0f02f7
omprakash1305 <ombshss2005@gmail.com>
2025-08-01 13:35:02 +0530

    Day 12

---
+++ C2TC_corejava/src/Day12/userdefinedexp/InvalidMarksException.java
@@ -0,0 +1,14 @@
+package Day12.userdefinedexp;
+
+public class InvalidMarksException extends Exception{
+
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+
+	
+}
